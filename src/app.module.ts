c3224e707def3de690164d948572edb906899124
姜赟 <jiangyun@jiangyundeMac-mini.local>
2020-08-06 21:31:28 +0800

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './controllers/app.controller';
 import { LoggerProvider } from './providers/logger.provider'
-import { AppService } from './app.service';
+import { AppService } from './services/app.service';
 import { ConfigModule } from './config/config.module';
 import { DatabaseProvider } from './providers/database.provider';
 
